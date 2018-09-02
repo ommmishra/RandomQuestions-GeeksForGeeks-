467757cb7ee846ad5cd71761a80023624e5cc254
ommmishra <41461196+ommmishra@users.noreply.github.com>
2018-09-02 10:48:17 +0530

    Add files via upload

---
+++ grtr.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class grtr{
+
+
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n+1];
+        System.out.println("The array should contain only one missing element, all elemnts must either be positive or negative.");
+        for(int i =0; i < array.length -1; i++) {
+            System.out.println("Enter The element of the array in incrementing order(incrementing by 1).");
+            array[i] = sc.nextInt();
+        }
+
+    for(int i = 0; i<array.length; i++)
+    {
+
+        if(Math.abs(array[i]) !=1){
+            for(int j = array.length - 1; j >= 1; j--)
+            {
+                array[j] = array[j-1];
+
+            }
+            if(array[i]<0) {
+                array[i] =  1;
+                array[i] = -array[i+1];
+            }
+            else
+                array[i] = 1;
+
+            break;
+
+
+        }
+
+        if( Math.abs(array[i + 1]) != Math.abs(array[i]) +1){
+
+                for(int j = array.length - 1; j >= i+1; j--)
+                {
+                    array[j] = array[j-1];
+                }
+                if(array[i]<0) {
+                    array[i + 1] = Math.abs(array[i + 1]) + 1;
+                    array[i + 1] = -array[i+1];
+                }
+                else
+                    array[i+1] = array[i+1] +1;
+
+                break;
+            }
+
+
+    }
+       for(int j= 0; j< array.length; j++) {
+           System.out.print(array[j]);
+           System.out.print(" ");
+       }}
+
+
+    }
+
+
